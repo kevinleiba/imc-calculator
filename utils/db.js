af4648a6d51964706c9be823a93397e5a75fb6dc
kleiba <kleiba@student.42.fr>
2022-04-16 20:36:34 +0200

    Feature/db functions (#2)
    * Added jest
    
    * Formatted google file to only focus on login
    
    * can get all IMCs
    
    * Can now append imcs
    
    * moved folder into right directory
    
    * now using process.env to get variables

---
+++ utils/db.js
@@ -0,0 +1,49 @@
+const { google } = require("googleapis");
+
+/**
+ * Prints the infos of imc people:
+ * @see https://docs.google.com/spreadsheets/d/1s6x4aWAptcaM0qWjVHHRUqXuSJPZpdTZ8276AiLQZ84/edit
+ * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
+ */
+function listImcs(auth) {
+  return new Promise((resolve, reject) => {
+    const sheets = google.sheets({ version: "v4", auth });
+
+    sheets.spreadsheets.values.get(
+      {
+        spreadsheetId: "1s6x4aWAptcaM0qWjVHHRUqXuSJPZpdTZ8276AiLQZ84",
+        range: "IMC!A2:E",
+      },
+      (err, res) => {
+        if (err) return reject("The API returned an error: " + err);
+        const rows = res.data.values;
+        if (rows) return resolve(rows);
+        return reject("No data");
+      }
+    );
+  });
+}
+
+function appendImcs(auth, values) {
+  return new Promise((resolve, reject) => {
+    const requestBody = { values };
+    const sheets = google.sheets({ version: "v4", auth });
+
+    sheets.spreadsheets.values.append(
+      {
+        spreadsheetId: "1s6x4aWAptcaM0qWjVHHRUqXuSJPZpdTZ8276AiLQZ84",
+        range: "IMC!A2:E",
+        requestBody,
+        valueInputOption: "raw",
+      },
+      (err, _result) => {
+        if (err) return reject("The API returned an error: " + err);
+        else {
+          return resolve(values);
+        }
+      }
+    );
+  });
+}
+
+module.exports = { listImcs, appendImcs };
---
+++ utils/db.test.js
@@ -0,0 +1,103 @@
+const { google } = require("googleapis");
+const { listImcs, appendImcs } = require("./db");
+
+const mockGetValues = jest.fn();
+const mockAppendValues = jest.fn();
+
+jest.mock("googleapis", () => {
+  return {
+    google: {
+      sheets: jest.fn().mockImplementation(() => ({
+        spreadsheets: {
+          values: {
+            get: mockGetValues,
+            append: mockAppendValues,
+          },
+        },
+      })),
+    },
+  };
+});
+
+const AUTH = "sampleAuthString";
+
+const IMCs = [["Sample Test", "123", "123", "12/12/2012", "21"]];
+
+const SUCCESS_RESULT = {
+  config: {
+    /* ... */
+  },
+  data: {
+    /* ... */
+  },
+  headers: {
+    /* ... */
+  },
+  status: 200,
+  statusText: "OK",
+};
+
+describe("DB", () => {
+  describe("listImcs", () => {
+    test("Can get all IMC data", async () => {
+      mockGetValues.mockImplementationOnce(async (infos, callback) => {
+        expect(infos.range).toBe("IMC!A2:E");
+        callback(null, { data: { values: IMCs } });
+      });
+
+      const imcs = await listImcs(AUTH);
+
+      expect(google.sheets).toHaveBeenCalledWith({ version: "v4", auth: AUTH });
+      expect(imcs).toEqual(IMCs);
+    });
+
+    test("Correct error is rejected", async () => {
+      const error = "sample error";
+
+      mockGetValues.mockImplementationOnce(async (infos, callback) => {
+        expect(infos.range).toBe("IMC!A2:E");
+        callback(error, {});
+      });
+
+      return expect(listImcs(AUTH)).rejects.toEqual(
+        `The API returned an error: ${error}`
+      );
+    });
+
+    test("If no data, error is rejected", async () => {
+      mockGetValues.mockImplementationOnce(async (infos, callback) => {
+        expect(infos.range).toBe("IMC!A2:E");
+        callback(null, { data: { values: null } });
+      });
+
+      return expect(listImcs(AUTH)).rejects.toEqual("No data");
+    });
+  });
+
+  describe("appendImcs", () => {
+    test("Can append new IMCs", async () => {
+      mockAppendValues.mockImplementationOnce(async (infos, callback) => {
+        expect(infos.range).toBe("IMC!A2:E");
+        expect(infos.requestBody).toEqual({ values: IMCs });
+        expect(infos.valueInputOption).toBe("raw");
+
+        callback(null, SUCCESS_RESULT);
+      });
+
+      const imcs = await appendImcs(AUTH, IMCs);
+
+      expect(google.sheets).toHaveBeenCalledWith({ version: "v4", auth: AUTH });
+      expect(imcs).toEqual(IMCs);
+    });
+
+    test("Error is rejected", () => {
+      const error = "Sample Error";
+      mockAppendValues.mockImplementationOnce(async (infos, callback) => {
+        callback(error, { ...SUCCESS_RESULT, status: 400, statusText: "NOK" });
+      });
+      return expect(appendImcs(AUTH)).rejects.toEqual(
+        `The API returned an error: ${error}`
+      );
+    });
+  });
+});
