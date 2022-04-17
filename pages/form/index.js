4768938c12184d47d5a4d2014c97c21f04c9fb5a
Kevin Leiba <k.leiba@free.fr>
2022-04-17 12:41:03 +0200

    Added style to Form page

--- pages/form/index.js
+++ pages/form/index.js
@@ -1,14 +1,31 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+import Button from "../../components/button";
+
+import style from "./index.module.css";
 
 export default function Form() {
   return (
-    <div className="container">
+    <div className={style.Form}>
       <Head>
         <title>BMI Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>In order to calculate your BMI, we need the following informations:</p>
-      <form action="/api/submit_data" method="post">
+      <Link href="/">
+        <a>
+          <Image src="/bmi.svg" alt="bmi logo" width="130" height="42.29" />
+        </a>
+      </Link>
+      <p className={style.Form__instructions}>
+        In order to calculate your BMI, we need the following informations:
+      </p>
+      <form
+        action="/api/submit_data"
+        className={style.Form__form}
+        method="post"
+      >
         <input type="text" id="name" name="name" placeholder="Name" required />
         <input
           required
@@ -35,7 +52,9 @@ export default function Form() {
           name="birthDate"
           placeholder="Birthdate (dd/mm/yyyy)"
         />
-        <button type="submit">Bring it on!!!</button>
+        <Button type="submit">
+          <p>Bring it on!!!</p>
+        </Button>
       </form>
     </div>
   );
