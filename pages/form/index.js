d130fd63495a5b1e1f1d0387ae09d7aa0aa9dfbb
Kevin Leiba <k.leiba@free.fr>
2022-04-17 12:17:00 +0200

    Basic form with basic validation

--- pages/form/index.js
+++ pages/form/index.js
@@ -4,10 +4,39 @@ export default function Form() {
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>BMI Calculator</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <p>Form</p>
+      <p>In order to calculate your BMI, we need the following informations:</p>
+      <form action="/api/submit_data" method="post">
+        <input type="text" id="name" name="name" placeholder="Name" required />
+        <input
+          required
+          type="number"
+          id="weight"
+          name="weight"
+          placeholder="Weight (kg)"
+          min={0}
+          max={1000}
+        />
+        <input
+          required
+          type="number"
+          min={40}
+          max={250}
+          id="size"
+          name="size"
+          placeholder="Size (cm)"
+        />
+        <input
+          required
+          type="text"
+          id="birthDate"
+          name="birthDate"
+          placeholder="Birthdate (dd/mm/yyyy)"
+        />
+        <button type="submit">Bring it on!!!</button>
+      </form>
     </div>
   );
 }
